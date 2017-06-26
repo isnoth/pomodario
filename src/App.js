416d92a33e9c5bfaf0f7ee6b7aa74d9ba8d1df55
isnoth <isnothe@gmail.com>
2017-06-26 22:18:35 +0800

    add an init mobx app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {observable, action} from 'mobx';
+import {observer} from 'mobx-react';
+
+var appState = observable({
+      timer: 0
+});
+
+
+appState.resetTimer = action(function reset() {
+    appState.timer = 0;
+});
+
+setInterval(action(function tick() {
+    appState.timer += 1;
+}), 1000)
+
+
+@observer
+class TimerView extends React.Component {
+    render() {
+        return (<button onClick={this.onReset.bind(this)}>
+                Seconds passed: {this.props.appState.timer}
+            </button>);
+    }
+
+    onReset () {
+        this.props.appState.resetTimer();
+    }
+};
+
+
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +45,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <TimerView appState={appState}/>
       </div>
     );
   }
