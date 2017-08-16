d134669919946033582cbfef6e8178fe4d13fe09
isnoth <isnothe@gmail.com>
2017-08-16 22:33:38 +0800

    add bullet

--- src/components/tree/tree.js
+++ src/components/tree/tree.js
@@ -4,6 +4,46 @@ import { noteStore, settingStore } from '../../store/store'
 
 import { TreeContent } from './tree-content'
 import { TreeContentVim } from './tree-content-vim'
+import {Glyphicon} from 'react-bootstrap'
+
+import '../../styles/tree.less'
+
+
+class Bullet extends Component {
+  constructor(props){
+    super(props)
+    this.state = {hot: false}
+  }
+
+  updateFold(){ }
+  drag(){}
+
+  render(){
+    const {_key} = this.props
+
+    console.log('Bullet:', _key, noteStore)
+    const hashHistory = []
+    const nodeUrl = ''
+    const data = noteStore.json
+
+    return (<div className="node-btn-wrap" 
+      onMouseEnter ={this.setHot}>
+      {data[_key].children?<Glyphicon className="fold" glyph={data[_key].fold?"plus":"minus"} onClick={this.updateFold.bind(this, !data[_key].fold)}/>:null}
+      <div className={data[_key].md?"dot-md":"dot"}></div>
+      {data[_key].fold?<div className="dot-fold" ></div>:null}
+      {this.state.hot?<div 
+        draggable='true' 
+        className="dot-hot" 
+        onDragStart={this.drag.bind(this, _key)}
+        onClick={()=>{ hashHistory.push(nodeUrl) }}
+        ></div>:null}
+        {/*<TreeMenu show={this.state.showMenu} nodeUpdate={this.nodeUpdate} node={content[_key]} onMouseBlur={this.clearHot}/>*/}
+    </div>)
+  }
+}
+
+
+
 
 @observer
 class Tree extends Component {
@@ -35,9 +75,8 @@ class Tree extends Component {
       <TreeContent _key={_key} {...this.state}/>
 
     return (
-    <div 
-      className="tree-node" >
-
+    <div className="tree-node" >
+      <Bullet {...this.props}/>
       {treeContent}
       <div tabIndex={-1}>
         {children}
