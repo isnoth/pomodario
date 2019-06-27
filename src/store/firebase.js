efa0eed1d8f5c78d32004d2656529d53f6b5ff29
isnoth <isnothe@gmail.com>
2019-06-27 12:18:29 +0800

    wilddog is down, use firebase instead

--- src/store/firebase.js
+++ src/store/firebase.js
@@ -1,34 +1,39 @@
-var Wilddog = require("wilddog");
+import firebase from "firebase";  
 
-var config
+const firebaseConfig_dev = {
+    apiKey: "AIzaSyDMGya80Zqfmo7_VpFjUg7vrpV8GfS9pgw",
+    authDomain: "bn5x-dev.firebaseapp.com",
+    databaseURL: "https://bn5x-dev.firebaseio.com",
+    projectId: "bn5x-dev",
+    storageBucket: "",
+    messagingSenderId: "895661616341",
+    appId: "1:895661616341:web:a3bb57b24ede661a"
+};
 
-if('development' === process.env.NODE_ENV){
-  config = {
-    syncURL: "https://bn5.wilddogio.com",
-    websocketOnly: false,
-    authDomain: "bn5.wilddog.com"
-  }
-}else{
-  config = {
-    syncURL: "https://bn5s.wilddogio.com",
-    websocketOnly: false,
-    authDomain: "bn5s.wilddog.com"
-  }
-}
+const firebaseConfig_prod = {
+    apiKey: "AIzaSyACjJ3HisGwDpo7pMAQr5elGGv57FEpWcQ",
+    authDomain: "bn5x-prod.firebaseapp.com",
+    databaseURL: "https://bn5x-prod.firebaseio.com",
+    projectId: "bn5x-prod",
+    storageBucket: "bn5x-prod.appspot.com",
+    messagingSenderId: "1050234679128",
+    appId: "1:1050234679128:web:827a3de9f64d57c5"
+};
 
-Wilddog.initializeApp(config);
+const isDev = 'development' === process.env.NODE_ENV
 
-const root = Wilddog.sync().ref();
+// Initialize Firebase
+const proj = firebase.initializeApp(isDev? firebaseConfig_dev : firebaseConfig_prod);
 
+const root = firebase.database().ref();
+const todos = firebase.database().ref('test');
 
-const todos = Wilddog.sync().ref('test');
-
-window.wilddog = Wilddog
+window.wilddog = firebase
 window.todos = todos
 window.rootRef = root
 
 const Fb = {
-  Wilddog,
+  Wilddog: firebase,
   root,
   todos
 };
